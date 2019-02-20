b6175bd8e51bffc070c9dc7f1469e4c638bab389
Daniel Betancurth <danielbetancurth@MacBook-Pro-de-Daniel.local>
2019-02-19 23:35:11 -0500

    Parte en casa para revisar

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,18 +1,28 @@
 package controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
+
+import com.opencsv.CSVReader;
+
 import model.data_structures.*;
 import model.util.Sort;
 import model.vo.VOMovingViolation;
 import view.MovingViolationsManagerView;
 
-@SuppressWarnings("unused")
 public class Controller {
+	private static final String enero = "./data/Moving_Violations_Issued_in_January_2018-2.csv";
+	private static final String febrero = "./data/Moving_Violations_Issued_in_February_2018.csv";
+	private static final String marzo = "./data/Moving_Violations_Issued_in_March_2018.csv";
+
 
 	private MovingViolationsManagerView view;
-	
+
 	// TODO Definir las estructuras de datos para cargar las infracciones del periodo definido
-	
+	private Cola<VOMovingViolation> queue;
+	private Pila<VOMovingViolation> stack;
 	// Muestra obtenida de los datos cargados 
 	Comparable<VOMovingViolation> [ ] muestra;
 
@@ -21,8 +31,10 @@ public class Controller {
 
 	public Controller() {
 		view = new MovingViolationsManagerView();
-		
+
 		//TODO inicializar las estructuras de datos para la carga de informacion de archivos
+		queue=new Cola<VOMovingViolation>();
+		stack=new Pila<VOMovingViolation>();
 	}
 
 	/**
@@ -33,10 +45,69 @@ public class Controller {
 	 */
 	public int loadMovingViolations() {
 		// TODO Los datos de los archivos deben guardarse en la Estructura de Datos definida
-		
-		return 0;
+		int contador=0;
+		try {
+			CSVReader lector = new CSVReader(new FileReader(enero));
+			String[] nextLineR1 = lector.readNext();
+			while((nextLineR1 = lector.readNext()) != null){
+				String id = nextLineR1[0];
+				int idObjeto = Integer.parseInt(id);
+				String location = nextLineR1[2];
+				String fecha = nextLineR1[13];
+				String fechaFormato = formatearFecha(fecha);
+				String total = nextLineR1[8];
+				int totalObjeto = Integer.parseInt(total);
+				String indicator = nextLineR1[12];
+				String description = nextLineR1[15];
+				queue.enqueue(new VOMovingViolation(idObjeto, location, fechaFormato, totalObjeto, indicator, description));
+				stack.push(new VOMovingViolation(idObjeto, location, fecha, totalObjeto, indicator, description));
+				contador++;
+			}
+			lector.close();
+			CSVReader lector2 = new CSVReader(new FileReader(febrero));
+			String[] nextLineR2 = lector2.readNext();
+			while((nextLineR2 = lector2.readNext()) != null){
+				String id = nextLineR2[0];
+				int idObjeto = Integer.parseInt(id);
+				String location = nextLineR2[2];
+				String fecha = nextLineR2[13];
+				String fechaFormato = formatearFecha(fecha);
+				String total = nextLineR2[8];
+				int totalObjeto = Integer.parseInt(total);
+				String indicator = nextLineR2[12];
+				String description = nextLineR2[15];
+				queue.enqueue(new VOMovingViolation(idObjeto, location, fechaFormato, totalObjeto, indicator, description));
+				stack.push(new VOMovingViolation(idObjeto, location, fecha, totalObjeto, indicator, description));
+				contador++;
+			}
+			lector2.close();
+			CSVReader lector3 = new CSVReader(new FileReader(marzo));
+			String[] nextLineR3 = lector3.readNext();
+			while((nextLineR3 = lector3.readNext()) != null){
+				String id = nextLineR3[0];
+				int idObjeto = Integer.parseInt(id);
+				String location = nextLineR3[2];
+				String fecha = nextLineR3[13];
+				String fechaFormato = formatearFecha(fecha);
+				String total = nextLineR3[8];
+				int totalObjeto = Integer.parseInt(total);
+				String indicator = nextLineR3[12];
+				String description = nextLineR3[15];
+				queue.enqueue(new VOMovingViolation(idObjeto, location, fechaFormato, totalObjeto, indicator, description));
+				stack.push(new VOMovingViolation(idObjeto, location, fecha, totalObjeto, indicator, description));
+				contador++;
+			}
+			lector3.close();
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return contador;
 	}
-	
+
 	/**
 	 * Generar una muestra aleatoria de tamaNo n de los datos leidos.
 	 * Los datos de la muestra se obtienen de las infracciones guardadas en la Estructura de Datos.
@@ -46,13 +117,16 @@ public class Controller {
 	public Comparable<VOMovingViolation> [ ] generarMuestra( int n )
 	{
 		muestra = new Comparable[ n ];
-					
+
 		// TODO Llenar la muestra aleatoria con los datos guardados en la estructura de datos
-		
+		for(int i=0;i<n;i++)
+		{
+			muestra[i]=queue.dequeue();
+		}
 		return muestra;
-		
+
 	}
-	
+
 	/**
 	 * Generar una copia de una muestra. Se genera un nuevo arreglo con los mismos elementos.
 	 * @param muestra - datos de la muestra original
@@ -65,16 +139,16 @@ public class Controller {
 		{    copia[i] = muestra[i];    }
 		return copia;
 	}
-	
+
 	/**
 	 * Ordenar datos aplicando el algoritmo ShellSort
 	 * @param datos - conjunto de datos a ordenar (inicio) y conjunto de datos ordenados (final)
 	 */
 	public void ordenarShellSort( Comparable<VOMovingViolation>[ ] datos ) {
-		
+
 		Sort.ordenarShellSort(datos);
 	}
-	
+
 	/**
 	 * Ordenar datos aplicando el algoritmo MergeSort
 	 * @param datos - conjunto de datos a ordenar (inicio) y conjunto de datos ordenados (final)
@@ -101,146 +175,162 @@ public class Controller {
 	public void invertirMuestra( Comparable[ ] datos ) {
 
 		// TODO implementar
+		Pila<VOMovingViolation> s = null;
+		for(int i=0;i<datos.length;i++)
+		{
+			s.push((VOMovingViolation) datos[i]);
+		}
+		for(int i=0;i<datos.length;i++)
+		{
+			datos[i]=s.pop();
+		}
 	}
-	
+
+
 	public void run() {
 		long startTime;
 		long endTime;
 		long duration;
-		
+
 		int nDatos = 0;
 		int nMuestra = 0;
-		
+
 		Scanner sc = new Scanner(System.in);
 		boolean fin = false;
-		
+
 		while(!fin)
 		{
 			view.printMenu();
-			
+
 			int option = sc.nextInt();
-			
+
 			switch(option)
 			{
-				case 1:
-					// Cargar infracciones
-					nDatos = this.loadMovingViolations();
-					view.printMensage("Numero infracciones cargadas:" + nDatos);
-					break;
-					
-				case 2:
-					// Generar muestra de infracciones a ordenar
-					view.printMensage("Dar tamaNo de la muestra: ");
-					nMuestra = sc.nextInt();
-					muestra = this.generarMuestra( nMuestra );
-					view.printMensage("Muestra generada");
-					break;
-					
-				case 3:
-					// Mostrar los datos de la muestra actual (original)
-					if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
-					{    
-						view.printDatosMuestra( nMuestra, muestra);
-					}
-					else
-					{
-						view.printMensage("Muestra invalida");
-					}
-					break;
+			case 1:
+				// Cargar infracciones
+				nDatos = this.loadMovingViolations();
+				view.printMensage("Numero infracciones cargadas:" + nDatos);
+				break;
 
-				case 4:
-					// Aplicar ShellSort a una copia de la muestra
-					if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
-					{
-						muestraCopia = this.obtenerCopia(muestra);
-						startTime = System.currentTimeMillis();
-						this.ordenarShellSort(muestraCopia);
-						endTime = System.currentTimeMillis();
-						duration = endTime - startTime;
-						view.printMensage("Ordenamiento generado en una copia de la muestra");
-						view.printMensage("Tiempo de ordenamiento ShellSort: " + duration + " milisegundos");
-					}
-					else
-					{
-						view.printMensage("Muestra invalida");
-					}
-					break;
-					
-				case 5:
-					// Aplicar MergeSort a una copia de la muestra
-					if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
-					{
-						muestraCopia = this.obtenerCopia(muestra);
-						startTime = System.currentTimeMillis();
-						this.ordenarMergeSort(muestraCopia);
-						endTime = System.currentTimeMillis();
-						duration = endTime - startTime;
-						view.printMensage("Ordenamiento generado en una copia de la muestra");
-						view.printMensage("Tiempo de ordenamiento MergeSort: " + duration + " milisegundos");
-					}
-					else
-					{
-						view.printMensage("Muestra invalida");
-					}
-					break;
-											
-				case 6:
-					// Aplicar QuickSort a una copia de la muestra
-					if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
-					{
-						muestraCopia = this.obtenerCopia(muestra);
-						startTime = System.currentTimeMillis();
-						this.ordenarQuickSort(muestraCopia);
-						endTime = System.currentTimeMillis();
-						duration = endTime - startTime;
-						view.printMensage("Ordenamiento generado en una copia de la muestra");
-						view.printMensage("Tiempo de ordenamiento QuickSort: " + duration + " milisegundos");
-					}
-					else
-					{
-						view.printMensage("Muestra invalida");
-					}
-					break;
-											
-				case 7:
-					// Mostrar los datos de la muestra ordenada (muestra copia)
-					if ( nMuestra > 0 && muestraCopia != null && muestraCopia.length == nMuestra )
-					{    view.printDatosMuestra( nMuestra, muestraCopia);    }
-					else
-					{
-						view.printMensage("Muestra Ordenada invalida");
-					}
-					break;
-					
-				case 8:	
-					// Una muestra ordenada se convierte en la muestra a ordenar
-					if ( nMuestra > 0 && muestraCopia != null && muestraCopia.length == nMuestra )
-					{    
-						muestra = muestraCopia;
-						view.printMensage("La muestra ordenada (copia) es ahora la muestra de datos a ordenar");
-					}
-					break;
+			case 2:
+				// Generar muestra de infracciones a ordenar
+				view.printMensage("Dar tamaNo de la muestra: ");
+				nMuestra = sc.nextInt();
+				muestra = this.generarMuestra( nMuestra );
+				view.printMensage("Muestra generada");
+				break;
 
-				case 9:
-					// Invertir la muestra a ordenar
-					if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
-					{    
-						this.invertirMuestra(muestra);
-						view.printMensage("La muestra de datos a ordenar fue invertida");
-					}
-					else
-					{
-						view.printMensage("Muestra invalida");
-					}
+			case 3:
+				// Mostrar los datos de la muestra actual (original)
+				if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
+				{    
+					view.printDatosMuestra( nMuestra, muestra);
+				}
+				else
+				{
+					view.printMensage("Muestra invalida");
+				}
+				break;
 
-					break;
-					
-				case 10:	
-					fin=true;
-					sc.close();
-					break;
+			case 4:
+				// Aplicar ShellSort a una copia de la muestra
+				if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
+				{
+					muestraCopia = this.obtenerCopia(muestra);
+					startTime = System.currentTimeMillis();
+					this.ordenarShellSort(muestraCopia);
+					endTime = System.currentTimeMillis();
+					duration = endTime - startTime;
+					view.printMensage("Ordenamiento generado en una copia de la muestra");
+					view.printMensage("Tiempo de ordenamiento ShellSort: " + duration + " milisegundos");
+				}
+				else
+				{
+					view.printMensage("Muestra invalida");
+				}
+				break;
+
+			case 5:
+				// Aplicar MergeSort a una copia de la muestra
+				if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
+				{
+					muestraCopia = this.obtenerCopia(muestra);
+					startTime = System.currentTimeMillis();
+					this.ordenarMergeSort(muestraCopia);
+					endTime = System.currentTimeMillis();
+					duration = endTime - startTime;
+					view.printMensage("Ordenamiento generado en una copia de la muestra");
+					view.printMensage("Tiempo de ordenamiento MergeSort: " + duration + " milisegundos");
+				}
+				else
+				{
+					view.printMensage("Muestra invalida");
+				}
+				break;
+
+			case 6:
+				// Aplicar QuickSort a una copia de la muestra
+				if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
+				{
+					muestraCopia = this.obtenerCopia(muestra);
+					startTime = System.currentTimeMillis();
+					this.ordenarQuickSort(muestraCopia);
+					endTime = System.currentTimeMillis();
+					duration = endTime - startTime;
+					view.printMensage("Ordenamiento generado en una copia de la muestra");
+					view.printMensage("Tiempo de ordenamiento QuickSort: " + duration + " milisegundos");
+				}
+				else
+				{
+					view.printMensage("Muestra invalida");
+				}
+				break;
+
+			case 7:
+				// Mostrar los datos de la muestra ordenada (muestra copia)
+				if ( nMuestra > 0 && muestraCopia != null && muestraCopia.length == nMuestra )
+				{    view.printDatosMuestra( nMuestra, muestraCopia);    }
+				else
+				{
+					view.printMensage("Muestra Ordenada invalida");
+				}
+				break;
+
+			case 8:	
+				// Una muestra ordenada se convierte en la muestra a ordenar
+				if ( nMuestra > 0 && muestraCopia != null && muestraCopia.length == nMuestra )
+				{    
+					muestra = muestraCopia;
+					view.printMensage("La muestra ordenada (copia) es ahora la muestra de datos a ordenar");
+				}
+				break;
+
+			case 9:
+				// Invertir la muestra a ordenar
+				if ( nMuestra > 0 && muestra != null && muestra.length == nMuestra )
+				{    
+					this.invertirMuestra(muestra);
+					view.printMensage("La muestra de datos a ordenar fue invertida");
+				}
+				else
+				{
+					view.printMensage("Muestra invalida");
+				}
+
+				break;
+
+			case 10:	
+				fin=true;
+				sc.close();
+				break;
 			}
 		}
+	}
+	public String formatearFecha(String pFecha){
+
+		String fechaFormateada = pFecha.substring(0,10);
+
+		return fechaFormateada;
 	}
 
 }
--- src/model/vo/VOMovingViolation.java
+++ src/model/vo/VOMovingViolation.java
@@ -6,31 +6,41 @@ package model.vo;
 public class VOMovingViolation implements Comparable<VOMovingViolation> {
 
 	// TODO Definir los atributos de una infraccion
-	
+	private int id;
+	private String loc;
+	private String date;
+	private int total;
+	private String indicator;
+	private String description;
 
 	/**
 	 * Metodo constructor
 	 */
-	public VOMovingViolation(  )
-	{
-		// TODO Implementar
+	public VOMovingViolation(int pId, String pLoc, String pDate, int pTotal, String pIndicator, String pDescription){
+
+		id= pId;
+		loc = pLoc;
+		date = pDate;
+		total = pTotal;
+		indicator = pIndicator;
+		description = pDescription;
 	}	
-	
+
 	/**
 	 * @return id - Identificador unico de la infraccion
 	 */
 	public int objectId() {
 		// TODO Auto-generated method stub
-		return 0;
+		return id;
 	}	
-	
-	
+
+
 	/**
 	 * @return location - Direccion en formato de texto.
 	 */
 	public String getLocation() {
 		// TODO Auto-generated method stub
-		return "";
+		return loc;
 	}
 
 	/**
@@ -38,42 +48,53 @@ public class VOMovingViolation implements Comparable<VOMovingViolation> {
 	 */
 	public String getTicketIssueDate() {
 		// TODO Auto-generated method stub
-		return "";
+		return date;
 	}
-	
+
 	/**
 	 * @return totalPaid - Cuanto dinero efectivamente paga el que recibio la infraccion en USD.
 	 */
 	public int getTotalPaid() {
 		// TODO Auto-generated method stub
-		return 0;
+		return total;
 	}
-	
+
 	/**
 	 * @return accidentIndicator - Si hubo un accidente o no.
 	 */
 	public String  getAccidentIndicator() {
 		// TODO Auto-generated method stub
-		return "";
+		return indicator;
 	}
-		
+
 	/**
 	 * @return description - Descripcion textual de la infraccion.
 	 */
 	public String  getViolationDescription() {
 		// TODO Auto-generated method stub
-		return "";
+		return description;
 	}
 
 	@Override
 	public int compareTo(VOMovingViolation o) {
 		// TODO implementar la comparacion "natural" de la clase
-		return 0;
+		if(Integer.parseInt(this.getTicketIssueDate())>Integer.parseInt(o.getTicketIssueDate()))
+			return 1;
+		else if(Integer.parseInt(this.getTicketIssueDate())<Integer.parseInt(o.getTicketIssueDate()))
+			return -1;
+		else
+		{
+			if(this.objectId()>o.objectId())
+				return 1;
+			else if(this.objectId()<o.objectId())
+				return -1;
+			return 0;
+		}
 	}
-	
+
 	public String toString()
 	{
 		// TODO Convertir objeto en String (representacion que se muestra en la consola)
-		return "-";
+		return " "+objectId();
 	}
 }
