6be06f18e7561f54afa6db439d6a84fd83248abb
Gal117 <danielgalindoruiz7@gmail.com>
2019-02-23 12:06:50 -0500

    Implementado Arreglo dinamico

--- test/model/util/SortTest.java
+++ test/model/util/SortTest.java
@@ -10,17 +10,69 @@ import model.util.Sort;
 public class SortTest {
 
 	// Muestra de datos a ordenar
-	private Comparable[] datos;
-	
-	@Before
-	public void setUp() throws Exception{
-		System.out.println("Codigo de configuracion de muestra de datos a probar");
-	}
+		private Comparable[] datos;
 
-	@Test
-	public void test() {
-		Sort.ordenarMergeSort(datos);
-		fail("Not yet implemented");
-	}
+		protected static final int[] ARREGLO = {350, 383, 105, 233, 140, 266, 356, 236, 80, 360, 221, 241, 130, 244, 352, 446, 18, 98, 97, 396};
+
+
+		@Before
+		public void setUp() throws Exception{
+			datos=new Comparable[ARREGLO.length];
+			int pos=0;
+			for(int a:ARREGLO)
+			{
+				datos[pos]=a;
+				pos++;
+			}
+		}
+		public void estaOrdenado(Comparable [] a)
+		{
+			boolean orden=true;
+			for(int i=0;i+1<a.length && orden;i++)
+			{
+				if(a[i].compareTo(a[i+1])>0)
+				{
+					orden=false;
+				}
+				System.out.println(a[i]);
+			}
+			if(orden==false)
+			{
+				fail("El arreglo no esta ordenado");
+			}
+		}
+		@Test
+		public void testMerge() {
+			try {
+				setUp();
+				Sort.ordenarMergeSort(datos);
+				estaOrdenado(datos);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		@Test
+		public void testQuick() {
+			try {
+				setUp();
+				Sort.ordenarQuickSort(datos);
+				estaOrdenado(datos);	
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		@Test
+		public void testShell() {
+			try {
+				setUp();
+				Sort.ordenarShellSort(datos);
+				estaOrdenado(datos);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 }
